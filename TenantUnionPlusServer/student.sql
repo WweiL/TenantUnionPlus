cfc0bfe1059e44ff311a174703fdaa2886967689
WweiL <z920631580@gmail.com>
2018-03-22 21:47:29 -0500

    change student.sql default value

--- TenantUnionPlusServer/student.sql
+++ TenantUnionPlusServer/student.sql
@@ -1,12 +1,12 @@
 drop table if exists student;
 create table student(
   NetID char(10) not NULL default 'siebel999',
-  name varchar(20) not NULL default 'Wallace',
-  gender char(10) not NULL default 'none',
+  name varchar(20) not NULL default 'notSet',
+  gender char(10) not NULL default 'notSet',
   age integer(2) not NULL default 0,
-  info varchar(140) not NULL default 'none',
-  major char(20) not NULL default 'CS',
-  mailbox varchar(30) not NULL default 'none',
+  info varchar(140) not NULL default 'notSet',
+  major char(20) not NULL default 'notSet',
+  mailbox varchar(30) not NULL default 'notSet',
   phone_num integer(12) not NULL default 0,
   PRIMARY KEY (NetID)
 );
